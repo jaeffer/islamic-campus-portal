b15b380da7cae1ff7b2fd703daa863e21a5028a4
JaefferH <140898337+jafferhuss@users.noreply.github.com>
2026-08-11 22:02:36 +0300

    Include @vitejs/plugin-react to resolve dev mode React Refresh runtime error

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
       server: { entry: "src/server.ts" },
     }),
+    react(),
     tailwindcss(),
     tsconfigPaths(),
   ],
